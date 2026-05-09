e288e55b89853cfdb4b409a006d38651a66cd973
anujgupta2in <95157005+anujgupta2in@users.noreply.github.com>
2026-05-09 13:08:57 +0530

    Update mooring_processor.py

--- mooring_processor.py
+++ mooring_processor.py
@@ -57,7 +57,7 @@ class MooringSystemProcessor:
             pivot_table.replace(np.nan, '', inplace=True)
             pivot_table.replace('', -1, inplace=True)
             pivot_table = pivot_table.astype(int)
-            pivot_table = pivot_table.applymap(self.format_blank)
+            pivot_table = pivot_table.map(self.format_blank)
 
             self.pivot_table_resultMooringJobs = pivot_table
             self.styled_pivot_table_resultMooringJobs = self.pivot_table_resultMooringJobs.style\
