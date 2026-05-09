44c40d7a236ef15e323b5973063180c42290a1d9
anujgupta2in <95157005+anujgupta2in@users.noreply.github.com>
2026-05-09 13:11:05 +0530

    Refactor power distribution data processing methods

--- powerdist_processor.py
+++ powerdist_processor.py
@@ -60,7 +60,7 @@ class PowerDistSystemProcessor:
             pivot_table.replace(np.nan, '', inplace=True)
             pivot_table.replace('', -1, inplace=True)
             pivot_table = pivot_table.astype(int)
-            pivot_table = pivot_table.applymap(self.format_blank)
+            pivot_table = pivot_table.map(self.format_blank)
 
             self.pivot_table_resultpowerdistJobs = pivot_table
             self.styled_pivot_table_resultpowerdistJobs = self.pivot_table_resultpowerdistJobs.style\
