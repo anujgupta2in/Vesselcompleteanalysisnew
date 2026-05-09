5ee7da083bd4211e206d78191babd00508617fa2
anujgupta2in <95157005+anujgupta2in@users.noreply.github.com>
2026-05-09 13:16:13 +0530

    Update workshop_processor.py

--- workshop_processor.py
+++ workshop_processor.py
@@ -53,7 +53,7 @@ class WorkshopSystemProcessor:
             pivot_table.replace(np.nan, '', inplace=True)
             pivot_table.replace('', -1, inplace=True)
             pivot_table = pivot_table.astype(int)
-            pivot_table = pivot_table.applymap(self.format_blank)
+            pivot_table = pivot_table.map(self.format_blank)
 
             self.pivot_table_resultworkshopJobs = pivot_table
             self.styled_pivot_table_resultworkshopJobs = self.pivot_table_resultworkshopJobs.style\
